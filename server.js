d42b06b2b590e90a8ee3ce8d5ed35ef972b73f76
Yigit <y.alanyali@gmail.com>
2017-12-30 16:35:03 +0100

    heroku deploy

--- server.js
+++ server.js
@@ -129,6 +129,6 @@ app.post('/', function(req, res) {
     });
 })
 
-app.listen((process.env.PORT || 3000, function() {
-    console.log('Listening on port 3000...')
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port 3000')
 });
